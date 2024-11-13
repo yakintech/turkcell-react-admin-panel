bfc1de6842850748b46d29f007b00f37dfe136b3
cagatay.yildiz <cagatay@pixupplay.com>
2024-11-13 15:42:17 +0300

    Add login functionality: implement Login component and conditionally render routes based on authentication status

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,27 @@ import FacibilitiesRoutes from './pages/facibilities'
 import MatchRoutes from './pages/match'
 import Header from './components/layout-components/Header'
 import { Container } from '@mui/material'
+import Login from './pages/auth/Login'
 
 function App() {
+
+  var isLogin = true
+
+
   return <>
-    <Header />
-    <hr />
-    <Container sx={{ marginTop: 2 }}>
-      <Routes>
-        <Route path="/facibilities/*" element={<FacibilitiesRoutes />} />
-        <Route path='/match/*' element={<MatchRoutes />} />
-      </Routes>
-    </Container>
+    {
+      isLogin ? <>
+        <Header />
+        <hr />
+        <Container sx={{ marginTop: 2 }}>
+          <Routes>
+            <Route path="/facibilities/*" element={<FacibilitiesRoutes />} />
+            <Route path='/match/*' element={<MatchRoutes />} />
+          </Routes>
+        </Container>
+      </>
+        : <Login />
+    }
   </>
 }
 
---
+++ src/pages/auth/Login.tsx
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import MuiCard from '@mui/material/Card';
+import { styled } from '@mui/material/styles';
+
+const Card = styled(MuiCard)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    alignSelf: 'center',
+    width: '100%',
+    padding: theme.spacing(4),
+    gap: theme.spacing(2),
+    margin: 'auto',
+    [theme.breakpoints.up('sm')]: {
+        maxWidth: '450px',
+    },
+    boxShadow:
+        'hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px',
+    ...theme.applyStyles('dark', {
+        boxShadow:
+            'hsla(220, 30%, 5%, 0.5) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.08) 0px 15px 35px -5px',
+    }),
+}));
+
+const SignInContainer = styled(Stack)(({ theme }) => ({
+    height: 'calc((1 - var(--template-frame-height, 0)) * 100dvh)',
+    minHeight: '100%',
+    padding: theme.spacing(2),
+    [theme.breakpoints.up('sm')]: {
+        padding: theme.spacing(4),
+    },
+    '&::before': {
+        content: '""',
+        display: 'block',
+        position: 'absolute',
+        zIndex: -1,
+        inset: 0,
+        backgroundImage:
+            'radial-gradient(ellipse at 50% 50%, hsl(210, 100%, 97%), hsl(0, 0%, 100%))',
+        backgroundRepeat: 'no-repeat',
+        ...theme.applyStyles('dark', {
+            backgroundImage:
+                'radial-gradient(at 50% 50%, hsla(210, 100%, 16%, 0.5), hsl(220, 30%, 5%))',
+        }),
+    },
+}));
+
+export default function Login(props: { disableCustomTheme?: boolean }) {
+    const [emailError, setEmailError] = React.useState(false);
+    const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+    const [passwordError, setPasswordError] = React.useState(false);
+    const [passwordErrorMessage, setPasswordErrorMessage] = React.useState('');
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        if (emailError || passwordError) {
+            event.preventDefault();
+            return;
+        }
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+    return <>
+        <CssBaseline enableColorScheme />
+        <SignInContainer direction="column" justifyContent="space-between">
+            <Card variant="outlined">
+
+                <Typography
+                    component="h1"
+                    variant="h4"
+                    sx={{ width: '100%', fontSize: 'clamp(2rem, 10vw, 2.15rem)' }}
+                >
+                    Sign in
+                </Typography>
+                <Box
+                    component="form"
+                    onSubmit={handleSubmit}
+                    noValidate
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        width: '100%',
+                        gap: 2,
+                    }}
+                >
+                    <FormControl>
+                        <TextField
+                            error={emailError}
+                            helperText={emailErrorMessage}
+                            id="email"
+                            type="email"
+                            name="email"
+                            placeholder="your@email.com"
+                            autoComplete="email"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={emailError ? 'error' : 'primary'}
+                            sx={{ ariaLabel: 'email' }}
+                        />
+                    </FormControl>
+                    <FormControl>
+                        <TextField
+                            error={passwordError}
+                            helperText={passwordErrorMessage}
+                            name="password"
+                            placeholder="••••••"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                            autoFocus
+                            required
+                            fullWidth
+                            variant="outlined"
+                            color={passwordError ? 'error' : 'primary'}
+                        />
+                    </FormControl>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                    >
+                        Sign in
+                    </Button>
+                </Box>
+            </Card>
+        </SignInContainer>
+    </>
+}
