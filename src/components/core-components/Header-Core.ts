b685e0c77135c3ceec839109b0d0b7eb4e7e23ca
cagatay.yildiz <cagatay@pixupplay.com>
2024-11-15 10:24:15 +0300

    Add styled-components for header styling: create Title and Wrapper components, update List page with new layout, and modify index.html for body margin

---
+++ src/components/core-components/Header-Core.ts
@@ -0,0 +1,14 @@
+
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+
+
+export const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
--- src/pages/adminuser/list/index.tsx
+++ src/pages/adminuser/list/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Title, Wrapper } from '../../../components/core-components/Header-Core'
 
 function List() {
 
 
   return <>
+  <Wrapper>
+    <Title>List Page</Title>
+  </Wrapper>
     <h1>List Page</h1>
 
   </>
